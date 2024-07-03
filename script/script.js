f2feb63253c8205c3c99924ced3e82f118b32646
Anujith9048 <anujith.s@techversantinfotech.com>
2024-07-03 11:05:07 +0530

    FormValidation 100% completed

--- script/script.js
+++ script/script.js
@@ -1,17 +1,26 @@
+document.addEventListener('DOMContentLoaded', function() {
+        document.getElementById("mobNo").addEventListener('input', function(e) {
+            let value = e.target.value;
+            e.target.value = value.replace(/[^0-9]/g, '');
+        });
+});
+
 function addnums(){ 
     var firstName =document.getElementById("firstName").value;
     var lastName = document.getElementById("lastName").value;
     var adress =document.getElementById("adress").value;
-    var mobNo =document.getElementById("mobNo").value;
     var email =document.getElementById("email").value;
     var pass = document.getElementById("pass").value;
     var confirmPass = document.getElementById("conformPass").value;
-   
+    var mobNo =document.getElementById("mobNo").value;
+    var isValid = 0;
+
     if(firstName ===""){
         document.getElementById("firstError").innerText = "Enter first name";
     }
     else{
         document.getElementById("firstError").innerText = "";
+	isValid++;
     } 
 
     if(lastName ===""){
@@ -19,6 +28,7 @@ function addnums(){
     }
     else{
         document.getElementById("lastError").innerText = "";
+	isValid++;
     }
 
     if(adress ===""){
@@ -26,6 +36,7 @@ function addnums(){
     }
     else{
         document.getElementById("adressError").innerText = "";
+	isValid++;
     }
 
     if(mobNo ===""){
@@ -33,13 +44,18 @@ function addnums(){
     }
     else{
         document.getElementById("mobError").innerText = "";
+	isValid++;
     }
 
     if(email ===""){
         document.getElementById("emailError").innerText = "Enter email id";
     }
+    else if(!email.endsWith("@gmail.com")){
+        document.getElementById("emailError").innerText = "Email must end with @gmail.com";
+    }
     else{
         document.getElementById("emailError").innerText = "";
+	isValid++;
     }
 
     if(pass ===""){
@@ -47,6 +63,7 @@ function addnums(){
     }
     else{
         document.getElementById("passError").innerText = "";
+	isValid++;
     }
 
     if(confirmPass ===""){
@@ -54,13 +71,23 @@ function addnums(){
     }
     else{
         document.getElementById("confirmError").innerText = "";
+	isValid++;
     }
-       
-
     if(pass === confirmPass){
-       document.getElementById("confirmError").innerText = "";
+        let pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&])[A-Za-z\d@.#$!%*?&]{8,15}$/; 
+	if(pattern.test(pass)){
+	    document.getElementById("confirmError").innerText = "";
+	    isValid++;
+	}
+	else{
+	    document.getElementById("confirmError").innerText = "Your password is not strong!";
+	}
     }
     else{
        document.getElementById("confirmError").innerText = "Password incorrect!!!";
+    }
+    
+    if(isValid === 8){
+	alert("Login Successfull !");
     }
 }
